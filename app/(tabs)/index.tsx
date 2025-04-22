6ee944e9cce92225f6241b5ddf164e615a87ad67
AnonymousFreelancer17 <AnonymousFreelancer17@gmail.com>
2025-04-22 13:05:22 +0530

    reset-project initiated

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-
-export default function HomeScreen() {
-  return (
-    <View className='flex justify-center items-center bg-red-400 flex-1'>
-      <Text className='text-white'>Hello Index</Text>
-    </View>
-  );
-}
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 justify-center items-center bg-primary">
+      <Text className="text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
